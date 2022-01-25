6e92d099ba8dbb09f72f1c6f9eb81642ff90d813
Levente Ibrányi <ibranyilevente@gmail.com>
2022-01-25 12:37:24 +0100

    Regisztáció gomb működik

--- app/src/main/java/com/todo/Login.java
+++ app/src/main/java/com/todo/Login.java
@@ -2,13 +2,33 @@ package com.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button regBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg = new Intent(Login.this, Registration.class);
+                startActivity(reg);
+            }
+        });
+    }
+
+    private void init(){
+        regBtn = findViewById(R.id.reg_btn);
     }
 }
